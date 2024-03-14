0502f1fb5fc71a6144db3b208803a26d32aa394f
sreenivasaganesh3 <150334102+sreenivasaganesh3@users.noreply.github.com>
2024-03-14 23:52:46 +0530

    Delete resourcegroup.tf

--- resourcegroup.tf
+++
@@ -1,8 +0,0 @@
-#create resource group
-resource "azurerm_resource_group" "rg" {
-    name     = "${var.rgname}"
-    location = "${var.location}"
-    tags      = {
-        Environment = "terraform demo"
-    }
-}
