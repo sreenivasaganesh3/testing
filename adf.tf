4642a43974e8e6d5ef19d542102415093d62d02e
sreenivasaganesh3 <150334102+sreenivasaganesh3@users.noreply.github.com>
2024-03-14 23:52:23 +0530

    Update adf.tf

--- adf.tf
+++ adf.tf
@@ -1,3 +1,13 @@
+#create resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "${var.rgname}"
+    location = "${var.location}"
+    tags      = {
+        Environment = "terraform demo"
+    }
+}
+
+
 resource "azurerm_data_factory" "adf" {
   name                = "${var.adfname}"
   location            = "${var.location}"
